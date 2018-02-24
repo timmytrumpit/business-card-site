a3974917cfe5f4af6aa01c080152d6a6e7ffce33
timmytrumpit <timknott6@gmail.com>
2018-02-24 20:11:05 +1100

    Introduction to what I do.

---
+++ script.js
@@ -0,0 +1,24 @@
+$('#web').on('mouseover', function(){
+  $('#background').addClass('web-active');
+})
+$('#web').on('mouseout', function(){
+  $('#background').removeClass('web-active');
+})
+
+
+
+$('#ui').on('mouseover', function(){
+  $('#background').addClass('ui-active');
+})
+$('#ui').on('mouseout', function(){
+  $('#background').removeClass('ui-active');
+})
+
+
+
+$('#graphic').on('mouseover', function(){
+  $('#background').addClass('graphic-active');
+})
+$('#graphic').on('mouseout', function(){
+  $('#background').removeClass('graphic-active');
+})
