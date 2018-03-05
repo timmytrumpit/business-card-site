9065ac038bfefe53d08bec7c2e89439ac92b3871
timmytrumpit <timknott6@gmail.com>
2018-03-05 11:36:34 +1100

    linked logo, refined js(by AJ)

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 $('#web').on('mouseover', function(){
   $('#background').addClass('web-active');
+  $('#background').removeClass('ui-active');
+  $('#background').removeClass('graphic-active');
 })
-$('#web').on('mouseout', function(){
-  $('#background').removeClass('web-active');
-})
+// $('#web').on('mouseout', function(){
+//   $('#background').removeClass('web-active');
+//   $('#background').removeClass('graphic-active');
+// })
 
 
 
 $('#ui').on('mouseover', function(){
   $('#background').addClass('ui-active');
+  $('#background').removeClass('web-active');
+  $('#background').removeClass('graphic-active');
 })
-$('#ui').on('mouseout', function(){
-  $('#background').removeClass('ui-active');
-})
+// $('#ui').on('mouseout', function(){
+//   $('#background').removeClass('ui-active');
+// })
 
 
 
 $('#graphic').on('mouseover', function(){
   $('#background').addClass('graphic-active');
+  $('#background').removeClass('web-active');
+  $('#background').removeClass('ui-active');
 })
-$('#graphic').on('mouseout', function(){
-  $('#background').removeClass('graphic-active');
-})
+// $('#graphic').on('mouseout', function(){
+  // $('#background').removeClass('graphic-active');
+// })
